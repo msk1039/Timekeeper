25144f57dde8a7ed2ea656acbcf51fccb1410ac3
Mayank kadam <mayankkadam1039@gmail.com>
2024-07-17 20:04:39 +0530

    added themes logic

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,51 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,svelte}'],
-	theme: {
-		extend: {},
-			screens: {
-				'sm': {'max': '386px'},
-				'md': {'max': '586px'},
-				'lg': {'min': '586px', 'max': '786px'},
-				'xl': {'min': '786px'},
-			},
-	},
-	plugins: [require("daisyui")],
-	daisyui: {
-		themes: ["halloween"],
-	  },
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,svelte}"],
+  theme: {
+    extend: {},
+    screens: {
+      sm: { max: "386px" },
+      md: { max: "586px" },
+      lg: { min: "586px", max: "786px" },
+      xl: { min: "786px" },
+    },
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+		"halloween",
+      "light",
+      "dark",
+      "cupcake",
+      "bumblebee",
+      "emerald",
+      "corporate",
+      "synthwave",
+      "retro",
+      "cyberpunk",
+      "valentine",
+      
+      "garden",
+      "forest",
+      "aqua",
+      "lofi",
+      "pastel",
+      "fantasy",
+      "wireframe",
+      "black",
+      "luxury",
+      "dracula",
+      "cmyk",
+      "autumn",
+      "business",
+      "acid",
+      "lemonade",
+      "night",
+      "coffee",
+      "winter",
+      "dim",
+      "nord",
+      "sunset",
+    ],
+  },
+};
